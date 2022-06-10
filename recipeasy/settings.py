6f64c316095eb46152bf2506e81e070a328c8fd8
Samuel Dainton <dexterdainton@gmail.com>
2022-06-10 16:46:54 +0000

    Feat: add account authentication with allauth. Pull out and modify signup, login and logout templates.

--- recipeasy/settings.py
+++ recipeasy/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -48,6 +52,12 @@ INSTALLED_APPS = [
     'taggit',
     'django_summernote'
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
