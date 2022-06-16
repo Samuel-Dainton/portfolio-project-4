17bb301fbf9005b9bbd112fb5a8f7038ada2fb7f
Samuel Dainton <dexterdainton@gmail.com>
2022-06-16 12:58:35 +0000

    Fix: Index now displaying topics correctly.

--- home/models.py
+++ home/models.py
@@ -15,23 +15,26 @@ class Topic(models.Model):
         return self.name
 
 class Recipe(models.Model):
+    # relationships
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ManyToManyField(Topic,)
     title = models.CharField(max_length=200)
-    image = CloudinaryField('image', null=True, blank=True, default="recipe_image.png")
+    # basics
     prep_time = models.CharField(null=True, blank=True, max_length=50)
     cook_time = models.CharField(null=True, blank=True, max_length=50)
     difficulty = models.CharField(max_length=50)
     servings = models.CharField(max_length=50)
-    introduction = RichTextField(blank=True, null=True)
-    method = RichTextField(blank=True, null=True)
     calories = models.CharField(blank=True, null=True, max_length=50)
     fat = models.CharField(blank=True, null=True, max_length=50)
     carbs = models.CharField(blank=True, null=True, max_length=50)
     protein = models.CharField(blank=True, null=True, max_length=50)
     allergy_info = models.TextField(blank=True, null=True)
+    # housekeeping
     created = models.DateTimeField(auto_now_add=True)
-
+    # thirdparty
+    introduction = RichTextField(blank=True, null=True)
+    method = RichTextField(blank=True, null=True)
+    image = CloudinaryField('image', null=True, blank=True, default="recipe_image.png")    
     class Meta:
         ordering = ['-created']
 
@@ -40,9 +43,14 @@ class Recipe(models.Model):
 
 class Ingredients(models.Model):
     ingredient = models.CharField(max_length=50)
+    # TODO should it not be positive int field?
     quantity = models.CharField(max_length=10)
     unit = models.CharField(max_length=50, blank=True, null=True)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    
+    # TODO reset database change ingredients to singular
+    class Meta:
+        verbose_name_plural = 'Ingredients'
 
     def __str__(self):
         return self.ingredient
@@ -55,6 +63,7 @@ class Comments(models.Model):
 
     class Meta:
         ordering = ['-created']
+        verbose_name_plural = 'Comments'
 
     def __str__(self):
         return self.body[0:50]
