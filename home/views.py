e6f4e5fb39763e89d58084f079776a95c10a8fc9
Samuel Dainton <dexterdainton@gmail.com>
2022-06-16 09:34:50 +0000

    Fix: Page generation and links for recipe pages working.

--- home/views.py
+++ home/views.py
@@ -7,4 +7,5 @@ def home(request):
     return render(request, 'home/index.html', context)
 
 def recipe(request, title):
+    recipe = Recipe.objects.get(title=title)
     return render(request, 'home/recipe.html')
