0e1427fed87e44c50a5ceefde1fbf8fb3662c22d
Samuel Dainton <dexterdainton@gmail.com>
2022-07-03 17:59:02 +0000

    Feat: Tests for forms. Error: topic and allergy_info fields not accepting inputs, tests keep returning 'lunch/test/peanuts not a valid value'.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from .forms import RecipeForm, UserForm
 
-# Create your tests here.
+
+class TestRecipeForm(TestCase):
+
+    # Tests to check required fields are not blank
+
+    def test_recipe_title_is_required(self):
+        form = RecipeForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_recipe_method_is_required(self):
+        form = RecipeForm({'method': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('method', form.errors.keys())
+        self.assertEqual(
+            form.errors['method'][0], 'This field is required.'
+            )
+
+    def test_recipe_ingredient_is_required(self):
+        form = RecipeForm({'ingredient': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('ingredient', form.errors.keys())
+        self.assertEqual(
+            form.errors['ingredient'][0], 'This field is required.'
+            )
+
+    # Test to check non required fields do not have to be present
+
+    def test_non_required_fields_are_not_required(self):
+
+        form = RecipeForm(
+            {
+                'title': 'Test', 'description': 'Test',
+                'ingredient': 'Test', 'method': 'Test',
+                'topic': ['test'], 'allergy_info': ['test', 'test'],
+                'prep_time': '1', 'cook_time': '1', 'author': 'Test',
+                'difficulty': 'Easy', 'servings': '12'
+                }
+            )
+        print(form.errors)
+        self.assertTrue(form.is_valid())
+
+class TestCommentForm(TestCase):
+
+    # Test to check non required fields do not have to be present
+    
+    def test_non_required_fields_are_not_required(self):
+
+        form = RecipeForm()
+        self.assertTrue(form.is_valid())
--- recipeasy/settings.py
+++ recipeasy/settings.py
@@ -120,17 +120,16 @@ WSGI_APPLICATION = 'recipeasy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_COPPER_URL'))
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_COPPER_URL'))
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
