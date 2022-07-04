1d496d108967a78c946c27b8dd805fefe2478ed4
Samuel Dainton <dexterdainton@gmail.com>
2022-07-04 15:34:43 +0000

    Fix: form errors on recipe form are displayed. disabled submit button until form is processed to prevent two submissions.

--- home/test_views.py
+++ home/test_views.py
@@ -7,27 +7,8 @@ class TestViews(TestCase):
 
     def setUp(self):
         self.test_user = User.objects.create(
-            password='abc123', username='testuser', user=1
+            password='abc123', username='testuser'
             )
-
-        self.recipe = Recipe.objects.create(title='Test', author=self.test_user)
-        self.vegan_recipe = Recipe.objects.create(
-            title='Onion Gravy',
-            author= self.test_user
-            )
-        self.vegetarian_recipe = Recipe.objects.create(
-            title='Salad',
-            author= self.test_user
-            )
-
-        self.comment = Comment.objects.create(
-            body='Test Comment', recipe=self.recipe, user=self.test_user
-            )
-        self.client.login(username='testuser', password='abc123')
-
-    def test_user_pw(self):
-        checked = self.test_user.check_password('abc123')
-        self.assertTrue(checked)
 
     def test_get_home_page(self):
         response = self.client.get('')
@@ -36,6 +17,6 @@ class TestViews(TestCase):
 
     # Default get keeps returning 301, follow leads to extention templates.
     def test_get_create_recipe_page(self):
-        response = self.client.get('/create-recipe', follow=True)
+        response = self.client.get('/create-recipe')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/recipe_form.html')
