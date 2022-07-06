2b67de93417fc28d21c675850f8b71feadbd23a5
Samuel Dainton <dexterdainton@gmail.com>
2022-07-06 10:19:18 +0000

    Feat: install whitenoise for static files with heroku deployment.

--- recipeasy/settings.py
+++ recipeasy/settings.py
@@ -87,6 +87,8 @@ CKEDITOR_CONFIGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
