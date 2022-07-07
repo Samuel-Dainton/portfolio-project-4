f72633ef300da4526b0c0ea9932322e4d6f67d65
Samuel Dainton <dexterdainton@gmail.com>
2022-07-07 16:10:07 +0000

    Fix: url changes to try and fix static files.

--- recipeasy/urls.py
+++ recipeasy/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
+    re_path(r'^', include('app.urls')),
+    re_path(r'^', include('home.urls')),    
+    re_path(r'^', include('recipeasy.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
 ]
