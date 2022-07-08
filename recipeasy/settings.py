aa160c65aec17cbde1addd7dd5b2f432cb049c1c
Samuel Dainton <dexterdainton@gmail.com>
2022-07-08 08:28:31 +0000

    Feat: variable to change debug to true/false if in development, determined in env.py

--- recipeasy/settings.py
+++ recipeasy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['recipeasy-sam-dainton.herokuapp.com', 'localhost']
 
